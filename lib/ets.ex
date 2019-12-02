8f67a5e34b4cf0110b8ccd002a4624786011310e
Zach Daniel <zachary.s.daniel@gmail.com>
2019-12-02 17:45:05 -0500

    use exported type
    Co-Authored-By: Mike Binns <TheFirstAvenger@users.noreply.github.com>

--- lib/ets.ex
+++ lib/ets.ex
@@ -28,7 +28,9 @@ defmodule Ets do
   @type table_identifier :: table_name | table_reference
   @type match_pattern :: :ets.match_pattern()
   @type match_spec :: :ets.match_spec()
-  @type continuation :: :ets.continuation()
+  @type continuation :: end_of_table |
+       {:ets.table(), integer(), integer(), :ets.comp_match_spec(), list(), integer()} |
+       {:ets.table, any(), any(), integer(), :ets.comp_match_spec(), list(), integer(),  integer()}
   @type end_of_table :: :"$end_of_table"
 
   @doc """
