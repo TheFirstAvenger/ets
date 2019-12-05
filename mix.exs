cef0141717686e75b93981d4c084fcad18732700
TheFirstAvenger <mike@mikebinns.net>
2019-12-04 20:32:23 -0500

    v0.8.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ETS.MixProject do
   def project do
     [
       app: :ets,
-      version: "0.7.3",
+      version: "0.8.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
